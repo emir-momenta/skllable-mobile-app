fbfabfffebc2bb0af68334881b0670aecbb21793
emir-momenta <emir@momenta.network>
2025-07-16 19:59:06 -0600

    Fix Babel Expo Router Transpilation

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          unstable_allowlist: ['expo-router']
+        }
+      ]
+    ],
     plugins: [
       'nativewind/babel',
       'react-native-reanimated/plugin',
